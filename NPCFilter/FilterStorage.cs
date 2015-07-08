66394cf956010fc33568e2180eeb60ee3eccf916
Patrikk <patrikveres@hotmail.com>
2015-07-08 15:45:03 +0100

    Main commit

---
+++ NPCFilter/FilterStorage.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPCFilter
+{
+	public class FilterStorage
+	{
+		[Description("List of NPCs to prevent from spawning.")]
+		public List<int> FilteredNPCs = new List<int>();
+
+
+		public static FilterStorage Read(string path)
+		{
+			if (!File.Exists(path))
+				return new FilterStorage();
+			using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+			{
+				return Read(fs);
+			}
+		}
+
+		public static FilterStorage Read(Stream stream)
+		{
+			using (var sr = new StreamReader(stream))
+			{
+				var cf = JsonConvert.DeserializeObject<FilterStorage>(sr.ReadToEnd());
+				if (FilterRead != null)
+					FilterRead(cf);
+				return cf;
+			}
+		}
+
+		public void Write(string path)
+		{
+			using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Write))
+			{
+				Write(fs);
+			}
+		}
+
+		public void Write(Stream stream)
+		{
+			var str = JsonConvert.SerializeObject(this, Formatting.Indented);
+			using (var sw = new StreamWriter(stream))
+			{
+				sw.Write(str);
+			}
+		}
+
+		public static Action<FilterStorage> FilterRead;
+	}
+
+	
+}
---
+++ NPCFilter/NPCFilter.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using TerrariaApi.Server;
+using TShockAPI;
+using TShockAPI.DB;
+using Terraria;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace NPCFilter
+{
+	[ApiVersion(1, 18)]
+	public class NPCFilter : TerrariaPlugin
+	{
+		public override Version Version
+		{
+			get { return new Version(1,0); }
+		}
+
+		public override string Name
+		{
+			get { return "NPC Filter"; }
+		}
+
+		public override string Author
+		{
+			get { return "Patrikk"; }
+		}
+
+		public override string Description
+		{
+			get { return "Filters NPC spawning."; }
+		}
+
+		public NPCFilter(Main game)
+			: base(game)
+		{
+		}
+
+		public static FilterStorage filterStorage = new FilterStorage();
+		private List<int> Filtered = new List<int>();
+		public string path = Path.Combine(TShock.SavePath, "NPCFilter.json");
+
+		public override void Initialize()
+		{
+			TShockAPI.Commands.ChatCommands.Add(new Command("npcfilter", FilterNPC, "npcfilter"));
+			ServerApi.Hooks.NpcSpawn.Register(this, OnSpawn);
+			ServerApi.Hooks.NpcTransform.Register(this, OnTransform);
+			FilterStorage.FilterRead += OnFilterRead;
+
+			if (File.Exists(path))
+			{
+				 filterStorage = FilterStorage.Read(path);
+			}
+			filterStorage.Write(path);
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				ServerApi.Hooks.NpcSpawn.Deregister(this, OnSpawn);
+			}
+			base.Dispose(disposing);
+		}
+
+		public void OnFilterRead(FilterStorage file)
+		{
+			Filtered = file.FilteredNPCs;
+		}
+
+		private void FilterNPC(CommandArgs args)
+		{
+			if (args.Parameters.Count == 1 && args.Parameters[0] == "list")
+			{
+				args.Player.SendInfoMessage("Banned NPC IDs: " + string.Join(", ", Filtered) + ".");
+				return;
+			}
+			else if (args.Parameters.Count == 1 && args.Parameters[0] == "reload")
+			{
+				FilterStorage.Read(path);
+				args.Player.SendSuccessMessage("Attempted to reload NPCFliter list!");
+				return;
+			}
+			else if (args.Parameters.Count == 2)
+			{
+				NPC npc;
+				List<NPC> matchedNPCs = TShock.Utils.GetNPCByIdOrName(args.Parameters[1]);
+				if (matchedNPCs.Count == 0)
+				{
+					args.Player.SendErrorMessage("Invalid NPC: '{0}'!", args.Parameters[1]);
+					return;
+				}
+				else if (matchedNPCs.Count > 1)
+				{
+					TShock.Utils.SendMultipleMatchError(args.Player, matchedNPCs.Select(i => i.name));
+					return;
+				}
+				else
+				{
+					npc = matchedNPCs[0];
+				}
+
+				switch (args.Parameters[0])
+				{
+					case "add":
+						{
+							if (Filtered.Contains(npc.netID))
+							{
+								args.Player.SendErrorMessage("NPC ID {0} is already on the filter list!", npc.netID);
+								return;
+							}
+							Filtered.Add(npc.netID);
+							File.WriteAllText(path, JsonConvert.SerializeObject(filterStorage, Formatting.Indented));
+							args.Player.SendSuccessMessage("Successfully added NPC ID to filter list: {0}!", npc.netID);
+							break;
+						}
+					case "delete":
+					case "del":
+					case "remove":
+						{
+							if (!Filtered.Contains(npc.netID))
+							{
+								args.Player.SendErrorMessage("NPC ID {0} is not on the filter list!", npc.netID);
+								return;
+							}
+							Filtered.Remove(npc.netID);
+							File.WriteAllText(path, JsonConvert.SerializeObject(filterStorage, Formatting.Indented));
+							args.Player.SendSuccessMessage("Successfully removed NPC ID from filter list: {0}!", npc.netID);
+							break;
+						}
+					default:
+						{
+							args.Player.SendErrorMessage("Syntax: /npcfilter <add/remove> [name or ID]");
+							break;
+						}
+				}
+			}
+			else
+			{
+				args.Player.SendInfoMessage("Available NPCFilter commands:");
+				args.Player.SendInfoMessage("/npcfilter <add/remove> [name or ID]");
+				args.Player.SendInfoMessage("/npcfilter list");
+				args.Player.SendInfoMessage("/npcfilter reload");
+				return;
+			}
+			
+		}
+
+		private void OnTransform(NpcTransformationEventArgs args)
+		{
+			if (args.Handled)
+				return;
+			if (Filtered.Contains(Main.npc[args.NpcId].netID))
+			{
+				Main.npc[args.NpcId].active = false;
+			}
+		}
+
+		private void OnSpawn( NpcSpawnEventArgs args)
+		{
+			if (args.Handled)
+				return;
+			if (Filtered.Contains(Main.npc[args.NpcId].netID))
+			{
+				args.Handled = true;
+				Main.npc[args.NpcId].active = false;
+				args.NpcId = 200;
+			}
+		}
+	}
+}
